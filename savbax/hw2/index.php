0b3344fb9950393b321d4b6a240f600687ff4f99
savbax <savbax@mail.ru>
2020-10-26 15:05:55 +0700

    26-10

---
+++ savbax/hw2/index.php
@@ -0,0 +1,39 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>nepвoe испытание</title>
+<link rel="stylesheet" href="outlines-chollenge.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+<h1>веб-страница Визитка резюме</h1>
+</header>
+<main>
+<nav>Каминский Вадим Валерьевич
+Г. Новосибирск, ул. В.Высоцкого, 36/1, кВ. 254
+Моб. тел.: +79963783025 , E-mail: savbax@mail.ru
+26 лет, гражданство Республика Беларусь, женат
+ </nav>
+<section>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/hvljGfsjeqQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<p>Организация и должность
+
+КСУП Гравжишки
+Главный ветеринарный врач
+Заместитель директора по животноводству
+
+</p>
+</section>
+<aside>
+Место учебы
+Гродненский Государственный Аграрный Университет
+</aside>
+</main>
+  
+<footer>Водительское удостоверение категории B</footer>
+</body>
+</html>
